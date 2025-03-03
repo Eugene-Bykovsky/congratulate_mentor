c5f3a117b02a1667a2e2e507ebb877d4a07b356f
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2025-03-03 09:51:16 +0400

    add code in bot.py

---
+++ bot/bot.py
@@ -0,0 +1,29 @@
+from environs import Env
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import logging
+from asyncio import run
+
+from app.handlers import start_handlers, mentor_handlers
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    env = Env()
+    env.read_env()
+    TELEGRAM_BOT_TOKEN = env.str('TELEGRAM_BOT_TOKEN')
+    if not TELEGRAM_BOT_TOKEN:
+        logging.error("Токен Telegram-бота не указан! Укажите переменную "
+                      "окружения TELEGRAM_BOT_TOKEN.")
+        return
+    logging.info("Бот запущен!")
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_routers(start_handlers, mentor_handlers)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    run(main())
